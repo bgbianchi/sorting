b488e3789d999b11c67f003c1dcdcc36443ed3ea
Bruno Bianchi <bgbianchi@gmail.com>
2015-04-03 03:02:34 -0300

    Update CBTree.hs

--- hs/CBTree.hs
+++ hs/CBTree.hs
@@ -7,8 +7,8 @@ insert x Empty = Perfect x Empty Empty
 insert x (Perfect y Empty Empty) = Right y (Perfect x Empty Empty) Empty
 insert x (Perfect y l r) = Left y (insert x l) r 
 insert x (Left y l r) = case insert x l of
-							Perfect y' l' r' -> Right y (Perfect y' l' r') r
-							t -> Left y t r
+				Perfect y' l' r' -> Right y (Perfect y' l' r') r
+				t -> Left y t r
 insert x (Right y l r) = case insert x r of
-							Perfect y' l' r' -> Perfect y l (Perfect y' l' r')
-							t -> Right y l t
+				Perfect y' l' r' -> Perfect y l (Perfect y' l' r')
+				t -> Right y l t
