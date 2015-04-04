59aba301f6659498239cce061a0e1b9b7f2f8e89
Bruno Bianchi <bgbianchi@gmail.com>
2015-04-04 00:06:36 -0300

    Binary heaps in Haskell

---
+++ hs/BHeap.hs
@@ -0,0 +1,49 @@
+module BHeap where
+
+import Prelude hiding (Left, Right, last, drop)
+import CBTree hiding (insert)
+
+type BHeap a = CBTree a
+
+insert :: Ord a => a -> BHeap a -> BHeap a
+insert x Empty = Perfect x Empty Empty
+insert x (Perfect y Empty Empty) = Right (min x y) (Perfect (max x y) Empty Empty) Empty
+insert x (Perfect y l r) = Left (min x y) (insert (max x y) l) r 
+insert x (Left y l r) = case insert (max x y) l of
+				Perfect y' l' r' -> Right (min x y) (Perfect y' l' r') r
+				t -> Left (min x y) t r
+insert x (Right y l r) = case insert (max x y) r of
+				Perfect y' l' r' -> Perfect (min x y) l (Perfect y' l' r')
+				t -> Right (min x y) l t
+
+build :: Ord a => [a] -> BHeap a
+build = foldl (flip insert) Empty
+				
+push :: Ord a => BHeap a -> BHeap a
+push Empty = Empty
+push (Perfect x Empty Empty) = Perfect x Empty Empty
+push (Perfect x l r) = push' Perfect x l r
+push (Left x l r) = push' Left x l r
+push (Right x (Perfect y Empty Empty) Empty) = Right (min x y) (Perfect (max x y) Empty Empty) Empty
+push (Right x l r) = push' Right x l r
+
+push' :: Ord a => (a -> BHeap a -> BHeap a -> BHeap a) -> a -> BHeap a -> BHeap a -> BHeap a
+push' f x l r
+  | x <= rl && x <= rr = f x l r
+  | x <= rl && rr <= x = f rr l (push (setRoot x r)) 
+  | rl <= x && x <= rr = f rl (push (setRoot x l)) r 
+  | rl <= x && rr <= x && rl <= rr = f rl (push (setRoot x l)) r 
+  | otherwise = f rr l (push (setRoot x r)) 
+  where rl = root l;
+		rr = root r
+
+drop :: Ord a => BHeap a -> BHeap a
+drop Empty = Empty
+drop (Perfect _ Empty Empty) = Empty
+drop h = push (setRoot x h') 
+	where x = last h;
+		h' = dropLast h
+
+flatten :: Ord a => BHeap a -> [a]
+flatten Empty = []
+flatten h = root h : flatten (drop h)
--- hs/CBTree.hs
+++ hs/CBTree.hs
@@ -1,8 +1,10 @@
-import Prelude hiding (Left, Right)
+module CBTree where
+
+import Prelude hiding (Left, Right, last)
 
 data CBTree a = Empty | Perfect a (CBTree a) (CBTree a) | Left a (CBTree a) (CBTree a) | Right a (CBTree a) (CBTree a) deriving Show
 
-insert :: Ord a => a -> CBTree a -> CBTree a
+insert :: a -> CBTree a -> CBTree a
 insert x Empty = Perfect x Empty Empty
 insert x (Perfect y Empty Empty) = Right y (Perfect x Empty Empty) Empty
 insert x (Perfect y l r) = Left y (insert x l) r 
@@ -12,3 +14,35 @@ insert x (Left y l r) = case insert x l of
 insert x (Right y l r) = case insert x r of
 				Perfect y' l' r' -> Perfect y l (Perfect y' l' r')
 				t -> Right y l t
+
+last :: CBTree a -> a
+last Empty = error "Empty!"
+last (Perfect x Empty Empty) = x
+last (Perfect _ _ r) = last r
+last (Left _ l _) = last l
+last (Right _ l Empty) = last l
+last (Right _ l (Perfect _ _ _)) = last l
+last (Right _ _ r) = last r
+
+dropLast :: CBTree a -> CBTree a
+dropLast Empty = Empty
+dropLast (Perfect _ Empty Empty) = Empty
+dropLast (Perfect x l r) = Right x l (dropLast r)
+dropLast (Left x l r) = case dropLast l of
+				Perfect y' l' r' -> Perfect x (Perfect y' l' r') r
+				t -> Left x t r
+dropLast (Right x _ Empty) = Perfect x Empty Empty
+dropLast (Right x l (Perfect y l' r')) = Left x (dropLast l) (Perfect y l' r')
+dropLast (Right x l r) = Right x l (dropLast r)
+
+root :: CBTree a => a
+root Empty = error "Empty!"
+root (Perfect x _ _) = x
+root (Left x _ _) = x
+root (Right x _ _) = x
+
+setRoot :: a -> CBTree a -> CBTree a
+setRoot _ Empty = Empty
+setRoot x (Perfect _ l r) = Perfect x l r
+setRoot x (Left _ l r) = Left x l r
+setRoot x (Right _ l r) = Right x l r
